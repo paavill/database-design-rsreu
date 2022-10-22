f92ec62c4a43a7db185cc06751e5ef5b969c9bb3
paavill <pavelchist2001@gmail.com>
2022-10-23 02:34:01 +0300

    view and simple controller added

--- src/main/java/ru/rsreu/database_design_rsreu/DatabaseDesignRsreuApplication.java
+++ src/main/java/ru/rsreu/database_design_rsreu/DatabaseDesignRsreuApplication.java
@@ -2,6 +2,7 @@ package ru.rsreu.database_design_rsreu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class DatabaseDesignRsreuApplication {
---
+++ src/main/java/ru/rsreu/database_design_rsreu/MainController.java
@@ -0,0 +1,35 @@
+package ru.rsreu.database_design_rsreu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.rsreu.database_design_rsreu.model.User;
+import ru.rsreu.database_design_rsreu.model.UserRoleEnum;
+import ru.rsreu.database_design_rsreu.model.UserStatusEnum;
+import ru.rsreu.database_design_rsreu.repository.UserRepository;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/")
+    public String get(Model model) {
+        userRepository.save(new User(null, UserRoleEnum.USER, UserStatusEnum.ONLINE, "Steve"));
+        model.addAttribute("headers", Arrays.stream(User.class.getDeclaredFields()).map(it -> it.getName()).toList());
+        model.addAttribute("rows", userRepository.findAll().stream().map(it -> {
+            List<String> userDataColumned = new ArrayList<>();
+            userDataColumned.add(it.getId().toString());
+            userDataColumned.add(it.getUserRoleEnum().toString());
+            userDataColumned.add(it.getUserStatusEnum().toString());
+            userDataColumned.add(it.getName());
+            return userDataColumned;
+        }).toList());
+        return "home";
+    }
+}
